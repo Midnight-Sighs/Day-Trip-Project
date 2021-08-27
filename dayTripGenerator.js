c6082f46417f462ad915cad3d4d6771cf612c491
Midnight <arreiskurai@yahoo.com>
2021-08-27 10:52:37 -0700

    I think I have everything working properly and now I'm going to move on to trying to make it look the way I want it to.

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,15 +1,15 @@
-// XX As a developer, I want to make at least three commits. (to gitHub)
+// XXX As a developer, I want to make at least three commits. (to gitHub)
 // XXX As a user, I want a destinationNames to be randomly selected for my day trip. 
 // XXX As a user, I want a restaurant to be randomly selected for my day trip.
 // XXX As a user, I want a mode of transportation to be randomly selected for my day trip.
 // XXX As a user, I want a form of entertainment to be randomly selected for my day trip.
-//  As a user, I want to be able to randomly re-select a destinationNames, XXXrestaurant, XXXmode of transportation, and/or XXXform of entertainment if I don’t like one or more of those things.
+//  As a user, I want to be able to randomly re-select a XXXdestination, XXXrestaurant, XXXmode of transportation, and/or XXXform of entertainment if I don’t like one or more of those things.
 // XXX As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
 // XXX As a user, I want to display my completed trip in the console.
-// As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
+// X??As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
 
 //Three destinations
-let destinations = ["Gold Saucer", "The Normandy", "Sword Coast",];
+let destinations = ["Gold Saucer", "The Normandy", "Sword Coast"];
 
 //Gold Saucer Data (GS) - XXX edit to sound better
 let chocoboColors = ["yellow", "blue", "green", "black", "gold", "pink", "white"];
@@ -30,7 +30,7 @@ let dataNorm = {
     restaurant:["We made you a reservation for you to eat at the ship Galley.  The food isn't glorified, but it's edible and will fill you up!", "For your meal, you will be eating in Miranda's office.  She's new to cooking and we haven't tried it, but we're sure you'll enjoy.", "We made you a reservation at a fancy restaurant in the Citadel.  Beware that the prices are steep, but we've given you a 2% off coupon!", "We made sure to tell the crew to get you a Super Sludge for your meal!  It doesn't sound like much but when the galley is down or the ship is in active status, a filling and nutrious meal is always available!"]
 }
 
-//Sword Coast Data (SC) - X edit to sound better
+//Sword Coast Data (SC) - XXX edit to sound better
 let dataSC = {
     name: "You're going to be traveling to the Sword Coast.",
     transportation: ["You'll be getting there by wagon.  We don't know what kind of creatures will be pulling the wagon though.", "You'll be getting there by horseback.  Pure, simple, effective, and to the point!", "You'll be getting there by hiking!  We hope you have some nice boots to keep your feet safe, temperature controlled, and sturdy.", "You'll be getting there by hippogriff!  We're assured that the hippogriff you'll be riding was bred and raised in captivity and is a wonderfully suitable mount for a beginnger!"],
@@ -38,10 +38,11 @@ let dataSC = {
     restaurant: ["For your meal, we have made arrangements for you to visits Neverwinter's famed Driftwood Tavern.  Enjoy the finest dining the Sword Coast has to offer!  First drink is on us!", "For your meal, we arranged for you to go to the Blushing Mermaid in Baulder's Gate! You may even get some free entertainment as adventurers come through!", "For your meal, we offer you dinner and a show at the Five Flagons of Baulder's Gate!", "For your meal, we've arranged for a group of adventurers to let you sit down with them for the evening! They're great company!"]
 }
 
-let randomDestination
-let randomLocal
+let randomDestination;
+let randomLocal;
 function getStarted(){
     randomDestination = determineRandomLocation();
+    randomLocal = randomDestination;
     return randomLocal;
 }
 getStarted();
@@ -50,18 +51,20 @@ let destinationNames;
 let randomTrans;
 let randomEnt;
 let randomRest;
+let userChoice
 assignOtherRandomVariables();
 alert(`${destinationNames} ${randomTrans} ${randomEnt} ${randomRest}`);
 confirmTrip();
 
-function confirmTrip(){
-    let userChoice = confirm("We hope you like the itinerary that we here are Travel Fantasy have created for you!  If you're happy with all of our choices, click the 'OK' button below.  If you aren't okay with them...we don't know what's wrong with you.  But go ahead and click 'Cancel' and you will have the chance to change any part of the trip you'd like to.");
-    while(userChoice === false){
+function confirmTrip(userChoice){
+    userChoice = confirm(`We hope you like the itinerary that we here are Travel Fantasy have created for you!  If you're happy with all of our choices, click the 'OK' button below.  If you aren't okay with them...we don't know what's wrong with you.  But go ahead and click 'Cancel' and you will have the chance to change any part of the trip you'd like to.\n\nYour Ininerary:\n${destinationNames} ${randomTrans} ${randomEnt} ${randomRest}`);
+    if(userChoice === false){
         notHappyWithTrip();
+        confirmTrip(true);
     }
     if(userChoice === true){
         happyWithTrip();
-        console.log(`${destinationNames} ${randomTrans} ${randomEnt} ${randomRest}`);
+        userChoice = true;
     }
 }
 
@@ -110,9 +113,8 @@ function determineRandomRestaurant(){
     return randomRest;
 }
 function happyWithTrip(){
-    alert(`You confirm that you are going to ${destinationNames} ${randomTrans} ${randomEnt} ${randomRest}.  We hope you enjoy your trip!  Do not forget to rate and review "Travel Fantasy" BEFORE your trip!`);
-    
-    alert("Travel Fantasy is not to be held liable or responsible for any damages, injuries, or claims made during travel.  Enjoy.")
+    alert(`You confirm that you like your vacation options!  \n\nWe hope you enjoy your trip!  Do not forget to rate and review "Travel Fantasy" BEFORE your trip! \n\nTravel Fantasy is not to be held liable or responsible for any damages, injuries, or claims made during travel.  Enjoy!`)
+    console.log(`${destinationNames} ${randomTrans} ${randomEnt} ${randomRest}`);
 }
 function changeYourMindTrans(){
     let newTrans = determineRandomTransportation();
@@ -129,24 +131,31 @@ return newRest;
 function notHappyWithTrip(){
     let changeThings = prompt('Please input the part of the trip you would like to change.  You can select the destination itself, creating a whole new itinerary for yourself or you can try out a new "transportation", "entertainment", or "restaurant".');
     if(changeThings.toLowerCase() === "destination"){
-              
+        getStarted();
+        assignObjects();
+        assignOtherRandomVariables();
+        alert(`${destinationNames} ${randomTrans} ${randomEnt} ${randomRest}`);   
     }
     else if(changeThings.toLowerCase() === "transportation"){
         let newTrans = changeYourMindTrans();
         randomTrans = newTrans;
+        userChoice = undefined;
         return randomTrans;
     }
     else if(changeThings.toLowerCase() === "entertainment"){
         let newEnt = changeYourMindEnt();
         let randomEnt = newEnt;
+        userCoice = undefined;
         return randomEnt;
     }
     else if(changeThings.toLowerCase() === "restaurant"){
         let newRest = changeYourMindRest();
         let randomRest = newRest;
+        userChoice = undefined;
         return randomRest;
     }
     else{
         alert('Please type in "transportation", "entertainment", or "restaurant" (We here are Travel Fantasy know that these can be difficult words to spell.)');
     }
+    return userChoice;
 }
